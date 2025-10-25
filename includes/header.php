0cfbadfb3028f8fc3ba494fc4258df6b040fb1b9
Visvajeet S <116342825+visvajeet-s05@users.noreply.github.com>
2025-10-25 11:48:39 +0530

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,83 @@
+<?php 
+require_once 'php_action/core.php';
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Inventory Management System</title>
+
+	<link rel="stylesheet" href="assests/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assests/bootstrap/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="assests/font-awesome/css/font-awesome.min.css">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="custom/css/custom.css">
+
+	<link rel="stylesheet" href="assests/plugins/datatables/jquery.dataTables.min.css">
+
+  <link rel="stylesheet" href="assests/plugins/fileinput/css/fileinput.min.css">
+
+  <script src="assests/jquery/jquery.min.js"></script>
+  <link rel="stylesheet" href="assests/jquery-ui/jquery-ui.min.css">
+  <script src="assests/jquery-ui/jquery-ui.min.js"></script>
+
+  <script src="assests/bootstrap/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+
+	<nav class="navbar navbar-light navbar-static-top main-navbar">
+		<div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php" style="font-weight: 700; color: #333;">
+      	<i class="glyphicon glyphicon-check"></i>
+      	Inventory Management System
+      </a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">      
+
+      <ul class="nav navbar-nav navbar-right">        
+
+      	<li id="navDashboard"><a href="index.php"><i class="glyphicon glyphicon-list-alt"></i>  Dashboard</a></li>        
+        
+        <li id="navBrand"><a href="brand.php"><i class="glyphicon glyphicon-btc"></i>  Brand</a></li>        
+
+        <li id="navCategories"><a href="categories.php"> <i class="glyphicon glyphicon-th-list"></i> Category</a></li>        
+
+        <li id="navProduct"><a href="product.php"> <i class="glyphicon glyphicon-ruble"></i> Product </a></li>     
+
+        <li class="dropdown" id="navOrder">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> <i class="glyphicon glyphicon-shopping-cart"></i> Orders <span class="caret"></span></a>
+          <ul class="dropdown-menu">            
+            <li id="topNavAddOrder"><a href="orders.php?o=add"> <i class="glyphicon glyphicon-plus"></i> Add Orders</a></li>            
+            <li id="topNavManageOrder"><a href="orders.php?o=manord"> <i class="glyphicon glyphicon-edit"></i> Manage Orders</a></li>            
+          </ul>
+        </li> 
+
+        <li id="navReport"><a href="report.php"> <i class="glyphicon glyphicon-check"></i> Report </a></li>
+
+        <li class="dropdown" id="navSetting">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> <i class="glyphicon glyphicon-user"></i> <span class="caret"></span></a>
+          <ul class="dropdown-menu">            
+            <li id="topNavSetting"><a href="setting.php"> <i class="glyphicon glyphicon-wrench"></i> Setting</a></li>            
+            <li id="topNavLogout"><a href="logout.php"> <i class="glyphicon glyphicon-log-out"></i> Logout</a></li>            
+          </ul>
+        </li>        
+               
+      </ul>
+    </div></div></nav>
+
+	<div class="container">
